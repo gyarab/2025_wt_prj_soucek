92afbd9134cdc1d9747b3c66a9e31f9b67432bf1
Jan Souček <jan.soucek.s@gyarab.cz>
2026-02-13 09:58:31 +0100

    added application app

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l+5y*q&%_zr5h5v_1#a^&20^-xitdxemwbfx_n=ij!^z)!ny1j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['#']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
