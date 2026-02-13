b6d3e6a94f19b9fc806f94be51f2a93f9ca14ca0
Jan Souček <jan.soucek.s@gyarab.cz>
2026-02-13 10:19:57 +0100

    a

--- prj/app/views.py
+++ prj/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def render_homepage(request):
+    return render(request, 'home.html')
+
+def render_about(request):
+    return render(request, 'about.html')
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.render_homepage, name='homepage'),
+    path('about/', views.render_about, name='about'),
 ]
