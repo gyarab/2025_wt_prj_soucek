e9b1dc73f6c4ad051d14f56ab104c1e55c6d25aa
Jan Souček <jan.soucek.s@gyarab.cz>
2026-03-27 09:19:20 +0100

    a

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import User, Produkt, Objednavka, Mnozstvi, Recenze
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    search_fields = ('name', 'email')
+
+
+@admin.register(Produkt)
+class ProduktAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    search_fields = ('name',)
+
+
+@admin.register(Objednavka)
+class ObjednavkaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at')
+    search_fields = ('user__name',)
+
+
+@admin.register(Mnozstvi)
+class MnozstviAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'quantity')
+    search_fields = ('order__id', 'product__name')
+
+
+@admin.register(Recenze)
+class RecenzeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'user', 'rating', 'created_at')
+    search_fields = ('product__name', 'user__name')
